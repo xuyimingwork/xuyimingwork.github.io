8061612de0373abc157abdd79bdccd0e669c0187
xuyiming <xuyimingwork@foxmail.com>
2019-08-19 20:57:35 +0800

    添加自动创建文件脚本

---
+++ _posts/2019-07-16-inspect-hover-display-item-in-chrome.md
@@ -0,0 +1,18 @@
+---
+title: "在 chrome 中检查悬浮样式"
+categories:
+  - 技术
+tags:
+  - 前端
+  - chrome
+---
+
+# 问题
+
+# 解答
+
+# 参见
+
+- [Inspect Javascript Hover in Chrome Developer Tools](https://stackoverflow.com/questions/25510439/inspect-javascript-hover-in-chrome-developer-tools)
+
+- [How can Chrome's Dev Tools be used to inspect a hover action if the style changes back to normal when moving the cursor back to the Dev Tools window?](https://www.quora.com/How-can-Chromes-Dev-Tools-be-used-to-inspect-a-hover-action-if-the-style-changes-back-to-normal-when-moving-the-cursor-back-to-the-Dev-Tools-window)
---
+++ scripts/new-post.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path')
+const moment = require('moment');
+const argv = require('yargs').argv;
+
+const DATE_FORMAT = 'YYYY-MM-DD';
+const DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
+const POST_DIR = '_posts/';
+
+function getPostFileName() {
+  const DEFAULT_FILE_NAME = 'post';
+  const prefix = `${moment().startOf('day').format(DATE_FORMAT)}-`;
+  const postName = argv._.length ? argv._.join('-') : DEFAULT_FILE_NAME;
+  const suffix = '.md';
+  return `${prefix}${postName}${suffix}`;
+}
+
+function getPostFileContent() {
+  const banner = `---
+title: "标题"
+date: ${moment().format(DATE_TIME_FORMAT)}
+categories:
+  - 目录
+tags:
+  - 标签
+---\n`;
+  return banner;
+}
+
+function isExist(file) {
+  return new Promise(resolve => {
+    fs.access(file, fs.constants.F_OK, err => {
+      resolve(!err ? true : false);
+    });
+  });
+}
+
+function createPostFile(name, content) {
+  return isExist(name)
+    .then(isExist => {
+      // 若文件已存在，抛出错误
+      if (isExist) throw new Error(`${name} is exist!`);
+      
+      // 若不存在，创建文件
+      return create(name, content);
+    })
+
+  function create(file, content) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(file, content ? content : '', err => {
+        if (err) return reject(err);
+        console.log(`create file ${file}`);
+        resolve();
+      })
+    })
+  }
+}
+
+function main() {
+  isExist(POST_DIR)
+    .then(isExist => {
+      if (!isExist) fs.mkdirSync(POST_DIR);
+      return createPostFile(`${POST_DIR}${getPostFileName()}`, getPostFileContent());
+    })
+    .catch(err => console.error(err))
+}
+
+main();
